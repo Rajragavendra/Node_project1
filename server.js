47d90a9cfe5a119433878e30005132022c79b1c2
Rajragavendra <rajragavendrasoftsuave@gmail.com>
2024-11-13 15:27:07 +0530

    backend upload

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const router = require('../myNode-backend/Routers/routerindex')
-require('dotenv').config();
-const cors = require('cors')
+    const express = require('express')
+    const router = require('../backend/Routers/routerindex')
+    require('dotenv').config();
+    const cors = require('cors')
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+    const app = express()
+    app.use(cors())
+    app.use(express.json())
 
-const port = process.env.APP_PORT
+    const port = process.env.APP_PORT
 
-app.get('/get', (req, res) => {
-    res.send('Success')
-})
+    app.get('/get', (req, res) => {
+        res.send('Success')
+    })
 
-app.use(router);
+    app.use(router);
 
-app.listen(port, () => {
-    console.log('Server is running..', port)
-})
+    app.listen(port, () => {
+        console.log('Server is running..', port)
+    })
